70a440b86558bb750055653637a801f602d38b8b
scorbiclife <scorbiclife+git@gmail.com>
2026-02-02 01:31:49 +0000

    test: reset database before each ttest run

--- src/jestGlobalSetup.mjs
+++ src/jestGlobalSetup.mjs
@@ -1,4 +1,8 @@
-import { initApp } from "./app.js";
+import app from "#src/app.js";
+import models from "#root/models/index.js";
 
 export default async function globalSetup() {
+  await app.initApp();
+  await models.sequelize.sync({ force: true });
+  await app.destroyApp();
 }
