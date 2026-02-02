0e7836c2b1f8139336726d9b9fe1ad819f2c1564
scorbiclife <scorbiclife+git@gmail.com>
2026-02-02 22:10:04 +0000

    fix: broken index.js
    `index.js` was broken due to not reflecting breaking changes
    caused by a refactoring

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { initApp, destroyApp } = require("#src/app.js");
+const { initApp, getApp, destroyApp } = require("#src/app.js");
 const { APP_PORT } = require("#root/src/config/app.js");
 
 async function main() {
-  const app = await initApp();
+  await initApp();
+  const app = getApp();
 
   app.listen(APP_PORT, () => {
     console.log(`Listening on port ${APP_PORT}`);
